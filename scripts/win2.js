6fca35130fb6539dc5a9c9bc9ea537d374b1bce8
Xhufg <Minardi.alessio99@gmail.com>
2024-02-05 21:42:10 +0100

    games solved

--- scripts/win2.js
+++ scripts/win2.js
@@ -1,17 +1,22 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
-    const tx = await game.win();
+    const tx1 = await game.setX(25);
+    await tx1.wait()
+    const tx2 = await game.setY(25);
+    await tx2.wait()
+    const tx3 = await game.win();
+
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
-    const receipt = await tx.wait();
+    const receipt = await tx3.wait();
     console.log(receipt);
 }
 
